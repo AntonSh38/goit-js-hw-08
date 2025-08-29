0c34636d7a3f6ed36d6fd3d27e300554d3d48937
Anton Shuvalov <anshuvalov83@gmail.com>
2025-08-29 21:43:03 +0300

    finish task 5

--- js/task-5.js
+++ js/task-5.js
@@ -1,3 +1,17 @@
+const bodyEl = document.querySelector('body');
+const btnEl = document.querySelector('.change-color');
+const spanEl = document.querySelector('.color');
+
+btnEl.addEventListener('click', onButton);
+
+function onButton() {
+  const randomColor = getRandomHexColor();
+
+  bodyEl.style.backgroundColor = randomColor;
+
+  spanEl.textContent = randomColor;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
